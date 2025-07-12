c4d8a42539bd45be0aa3bbdb3c0bdcac021d5bcf
Artem Dobrodub <dobrodub@gmail.com>
2025-07-12 16:36:37 -0700

    refactor: Remove temporary debugging test

--- app/src/test/kotlin/org/example/AppTest.kt
+++ app/src/test/kotlin/org/example/AppTest.kt
@@ -34,9 +34,4 @@ class AppTest {
         val expected = LocalDate.of(2023, 10, 26)
         assertEquals(expected, parseUserDate("  26-10-2023  "))
     }
-
-    @Test fun `parseBulletinDate should handle ddMMMyy format`() {
-        val expected = LocalDate.of(2023, 4, 1)
-        assertEquals(expected, parseBulletinDate("01APR23"))
-    }
 }
